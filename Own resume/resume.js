975f3c505fc07c151b7c12417aed2892d448221d
Kavipriyan <144509912+KAVI7871281698@users.noreply.github.com>
2024-12-02 09:57:36 +0530

    Add files via upload

---
+++ Own resume/resume.js
@@ -0,0 +1,8 @@
+// window.onscroll = () => {
+//     if(scrollY  > 30){
+//      document.querySelector('.res1').style.backgroundColor = '#1b1514e7';
+//     }
+//     else if(scrollY === 0){
+//      document.querySelector('.res1').style.backgroundColor = 'transparent';
+//     }
+//  }
